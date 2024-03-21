02a50859dbf9bd01c8fc1a87b47fc498ec7bb4e2
nickl474 <linder.nick@ymail.com>
2024-03-20 22:41:31 -0700

    base code update
    update

--- midterm.c
+++ midterm.c
@@ -1,30 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-
-int main() {
-    const int search_max = 1000;
+#include <time.h>
+#include <omp.h>
+#include <chrono>
+// g++ <filename>.c -o <filename>.exe -fopenmp using namespace std::chrono;
+int main(int argc, char argv[])
+{
+    // clock_t begin = clock();
+    auto begin = steady_clock::now();
+    int search_max = strtol(argv[1], NULL, 10);
     int sqrt_search_max = (int)sqrt(search_max);
-
-    int primes[search_max];
-
-    for (int i = 0; i < search_max; i++) {
+    int thread_count = strtol(argv[2], NULL, 10);
+    intprimes;
+    primes = (int )malloc(search_max sizeof(int));
+    for (int i = 0; i < search_max; i++)
+    {
         primes[i] = i + 1;
     }
+#pragma omp parallel num_threads(thread_count)
+    {
+        for (int i = 1; i < sqrt_search_max; i++)
+        {
+            int current_prime = primes[i];
+            if (current_prime != 0)
+{
+#pragma omp for
 
-    for (int i =1; i < sqrt_search_max; i++) {
-        int current_prime = primes[i];
-        if (current_prime != 0) {
-            for (int j = i + current_prime; j < search_max; k += current_prime) {
-                primes[j] = 0
-            }
-        }
-    }
-
-    printf("The prime numbers are: ");
-    for (int i = 1; i < search_max; i++) {
-        if (primes[i] != 0) printf("%d ", primes[i]);
-    }
-
-    printf("End of program reached.\n");
-    return 0;
+ }
+} }
+for (int j = i + current_prime; j < search_max; j += current_prime)
+{
+    primes[j] = 0;
 }
+    // printf("The prime numbers are: ");
+    // for (int i = 1; i < search_max; i++)
+    // {
+    //     if (primes[i] != 0)
+    // }
+    free(primes);
+    printf("End of program reached.\n");
+    // clock_t end = clock();
+    auto end = steady_clock::now();
+auto time_spent = duration_cast<microseconds>(end - begin).count(); printf("The program took %lld microseconds to run.\n", time_spent); // double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+// printf("The program took %f seconds to run.\n", time_spent); return 0;
+}
+[10:19 PM]
+###################
